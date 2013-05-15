fd46637633f145fab857b52712d7afa7f7fa0e70
Skrimp Danielson <skrimp.danielson@gmail.com>
2013-05-15 09:41:34 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WitchesBrews::Application.config.secret_key_base = 'bbe49b8d622fffb875e3b591f088c4686377bdae71bd4eff48af4253626f3332672d000d0766b840299bf768798cc28029d2bdf259349310e2e101a95eb147a9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WitchesBrews::Application.config.session_store :encrypted_cookie_store, key: '_witches_brews_session'
